2be44309f0f961be4ee51afe1ae21e8ad81c901b
majian <2300929@stu.neu.edu.cn>
2025-02-28 20:35:39 +0800

    feat: 代码评审 v4.2

--- openai-code-review-test/src/test/java/plus/gaga/middleware/test/ApiTest.java
+++ openai-code-review-test/src/test/java/plus/gaga/middleware/test/ApiTest.java
@@ -14,7 +14,7 @@ public class ApiTest {
 
     @Test
     public void test() {
-        System.out.println(Integer.parseInt("1234"));
+        System.out.println(Integer.parseInt("aaaaaaa1234"));
     }
 
 }
